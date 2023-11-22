40c2781e4b0c7203e85f858cd0eca9314a8961f7
Elena <94857442+eenvk@users.noreply.github.com>
2023-11-22 19:49:28 +0100

    Update main.cpp

--- provaintermedia/main.cpp
+++ provaintermedia/main.cpp
@@ -5,40 +5,45 @@
 using namespace std;
 
 int main() {
-
-    cout<<"\n-------BOOKSHELF-------";
     Date d1= *new Date(10,12,2023);
 
-    Book mybook("Felicia", "Kingsley", "Una ragazza d'altri tempi", "341-127-009-1", Date(10,9,2023));
-    mybook.prendi();
+    Book mybook1("Felicia", "Kingsley", "Una ragazza d'altri tempi", "341-127-009-1",10,9,2023);
+    mybook1.prendi();
 
     Book mybook2("Anna", "Premoli", "Facciamo finta che mi ami", "667-432-178-4",d1);
 
-    Book mybook3("David", "Foster Wallace", "Una cosa divertente che non faro mai piu", "887-521-837-4",6,8,1997);
+    Book mybook3("David", "Foster Wallace", "Una cosa divertente che non faro mai piu", "887-521-837-4", Date(6,8,1997));
 
     Book mybook4("Alessia", "Gazzola", "Le ossa della principessa", "135-999-087-7");
 
-
     BookShelf shelf;
-
-    shelf.push_back(mybook);
+    shelf.push_back(mybook1);
     shelf.push_back(mybook2);
     shelf.push_back(mybook3);
     shelf.push_back(mybook4);
 
-    cout<< "\n"<<shelf;
+    cout<<shelf<<"\n";
 
 
-    cout<<"\n---------Elimino l'ultimo elemento--------";
+    cout<<"---------Elimino l'ultimo elemento--------"<<"\n";
     shelf.pop_back();
-    cout<< "\n"<<shelf;
+    cout<<shelf<<"\n";
 
-    cout<<"\n---------Controllo se mybook4 e mybook2 sono uguali--------";
+    cout<<"---------gioco con i bookshelf--------"<<"\n";
 
-    if(mybook4.operator==(mybook2)){
+    BookShelf shelf2;
+    cout<<shelf2;
+    shelf2=shelf;
+    cout<<shelf2<<endl;
+    cout<<shelf2[100]; //non stampa niente stampa
+
+    cout<<"---------Controllo se mybook4 e mybook2 sono uguali--------"<<"\n";
+
+    if(mybook4==mybook2){
         cout<<"\ni due libri sono uguali";
     }else{
         cout<<"\ni due libri non sono uguali";
     }
+
     return 0;
 }
