93343b7d9755ced9f9d9cad0a1aecf1074881516
EORNZ <edorenzi03@outlook.com>
2023-11-22 17:49:33 +0100

    ho modificato le cartelle

---
+++ provaintermedia/cmake-build-debug/CMakeFiles/Progetto.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/edore/Desktop/provaintermedia/provaintermedia/main.cpp" "CMakeFiles/Progetto.dir/main.cpp.obj" "gcc" "CMakeFiles/Progetto.dir/main.cpp.obj.d"
+  "C:/Users/edore/Desktop/provaintermedia/provaintermedia/src/Book.cpp" "CMakeFiles/Progetto.dir/src/Book.cpp.obj" "gcc" "CMakeFiles/Progetto.dir/src/Book.cpp.obj.d"
+  "C:/Users/edore/Desktop/provaintermedia/provaintermedia/src/BookShelf.cpp" "CMakeFiles/Progetto.dir/src/BookShelf.cpp.obj" "gcc" "CMakeFiles/Progetto.dir/src/BookShelf.cpp.obj.d"
+  "C:/Users/edore/Desktop/provaintermedia/provaintermedia/src/Date.cpp" "CMakeFiles/Progetto.dir/src/Date.cpp.obj" "gcc" "CMakeFiles/Progetto.dir/src/Date.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
