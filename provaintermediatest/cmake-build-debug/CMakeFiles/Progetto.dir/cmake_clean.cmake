fd744d66685e44c27ff36491d22a60a815bd1097
EORNZ <edorenzi03@outlook.com>
2023-11-22 15:17:26 +0100

    tolto la cartella inutile

---
+++ provaintermediatest/cmake-build-debug/CMakeFiles/Progetto.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Progetto.dir/main.cpp.obj"
+  "CMakeFiles/Progetto.dir/main.cpp.obj.d"
+  "CMakeFiles/Progetto.dir/src/Book.cpp.obj"
+  "CMakeFiles/Progetto.dir/src/Book.cpp.obj.d"
+  "CMakeFiles/Progetto.dir/src/BookShelf.cpp.obj"
+  "CMakeFiles/Progetto.dir/src/BookShelf.cpp.obj.d"
+  "CMakeFiles/Progetto.dir/src/Date.cpp.obj"
+  "CMakeFiles/Progetto.dir/src/Date.cpp.obj.d"
+  "Progetto.exe"
+  "Progetto.exe.manifest"
+  "Progetto.pdb"
+  "libProgetto.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Progetto.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
