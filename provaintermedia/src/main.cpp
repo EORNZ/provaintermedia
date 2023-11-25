803cde5622875a336447a92c63e3951a61fe890d
EORNZ <103444028+EORNZ@users.noreply.github.com>
2023-11-25 15:15:03 +0100

    main.cpp con parte inutile commentata sotto

--- provaintermedia/src/main.cpp
+++ provaintermedia/src/main.cpp
@@ -6,7 +6,84 @@ using namespace std;
 
 int main() {
 
+    Book mybook1("Felicia", "Kingsley", "Una ragazza d'altri tempi", "341-127-009-1");
+    mybook1.prendi();
+
     Date d1 = Date(10,12,2023);
+    Book mybook2("Anna", "Premoli", "Facciamo finta che mi ami", "667-432-178-4",d1);
+
+    Book mybook3("David", "Foster Wallace", "Una cosa divertente che non faro mai piu", "887-521-837-4", Date(6,8,1997));
+
+    Book mybook4("Alessia", "Gazzola", "Le ossa della principessa", "135-999-087-7");
+
+    //creo un bookshelf
+    BookShelf shelf = BookShelf(1);
+    shelf.push_back(Book("Felicia", "Kingsley", "Una ragazza d'altri tempi", "341-127-009-1",Date(10,9,2023)));
+    shelf.push_back(Book("David", "Foster Wallace", "Una cosa divertente che non faro mai piu", "887-521-837-4", 6,8,1997));
+
+    cout<<"----------------Stampo i libri contenuti in shelf ----------------\n"<<shelf<<endl;
+
+    cout<<"Modifico il libro 1\n";
+    shelf[0] = Book("Felicia", "Kingsley", "Matrimonio di convenienza", "222-222-222-1",Date(10,9,2017));
+
+    cout<<"----------------Stampo i libri contenuti in shelf ----------------\n"<<shelf<<endl;
+
+    cout<<"Prendo in prestito il libro 2\n";
+    shelf[1].prendi();
+
+    cout<<"----------------Stampo i libri contenuti in shelf ----------------\n"<<shelf<<endl;
+
+    cout<<"\n*****************************************\n"<<endl;
+
+
+    BookShelf shelf2 = BookShelf(2);
+    shelf2.push_back(mybook1);
+    shelf2.push_back(mybook2);
+    shelf2.push_back(mybook3);
+    cout<<"Stampo i libri contenuti in shelf2 \n"<<shelf2<<"\n";
+
+    cout<<"Modifico il libro 3\n";
+    shelf2[2] = mybook4;
+
+    cout<<"Stampo i libri contenuti in shelf2 \n"<<shelf2<<"\n";
+
+    cout<<"Elimino l'ultimo elemento"<<endl;
+    shelf2.pop_back();
+
+    cout<<"Stampo i libri contenuti in shelf2 \n"<<shelf2<<"\n";
+
+    cout<<"Modifico il libro 1\n";
+    shelf2[0] = shelf[0];
+
+    cout<<"----------------Stampo i libri contenuti in shelf2 ----------------\n"<<shelf2<<"\n";
+    cout<<"----------------Stampo i libri contenuti in shelf ----------------\n"<<shelf<<"\n";
+
+    cout<<"\n----------------Confronto due libri nella shelf----------------\n";
+
+    cout<<"libro selezionato: '"<<shelf[0].getTitolo()<<"' "<<endl;
+    cout<<"libro selezionato: '"<<shelf[1].getTitolo()<<"' "<<endl;
+
+    if(shelf[0]==shelf[1]){
+        cout<<"La libreria presenta 2 libri uguali\n";
+    }else{
+        cout<<"La libreria non presenta 2 libri uguali\n";
+    }
+
+    Book mybook5("Collen", "Hoover", "Ugly Love", "888-345-899-9");
+
+    BookShelf shelf3{mybook1, mybook2, mybook5};
+    shelf3[2].prendi(); //mybook5 da disponibile diventa in prestito
+    cout<<"\n----------------Creazione terza libreria: Shelf3, con differente inserimento----------------\n";
+    cout<<shelf3;
+
+
+    return 0;
+}
+
+
+//test main
+
+/*Date d1 = Date(10,12,2023);
 
     Book mybook1("Felicia", "Kingsley", "Una ragazza d'altri tempi", "341-127-009-1",10,9,2023);
     mybook1.prendi();
@@ -75,17 +152,6 @@ int main() {
     cout<<"shelf.prendi_libro(book) stampo shelf \n"<<shelfedo<<"\n";
     shelfedo.restituisci_libro(book);
     cout<<"shelf.restituisci_libro(book) stampo shelf \n"<<shelfedo<<"\n";
-*/
-
-    return 0;
-}
-
-
-//test main
-
-/*
-
-
  *
  *
  * -------------------------------------------------------------------------------------------
