7e37f9b279e30066804db48725e3515d0c5b1f13
Elena <94857442+eenvk@users.noreply.github.com>
2023-11-25 11:43:22 +0100

    Update Date.h

--- provaintermedia/include/Date.h
+++ provaintermedia/include/Date.h
@@ -3,26 +3,27 @@
 
 #include <iostream>
 
-
 class InvalidDate{};
 
 class Date{
-
 public:
     //costruttori
     Date();
     Date(int,int,int);
+
     //overloading operatori
     Date& operator=(const Date&);
     bool operator==(const Date&) const;
     bool operator!=(const Date&) const;
     bool operator<(const Date &) const;
     bool operator>(const Date &) const;
+
     //ausiliari
     int getGiorno() const;
     int getMese() const;
     int getAnno() const;
     bool isIgnoto() const;
+
     //toString
     std::string toString() const;
 
